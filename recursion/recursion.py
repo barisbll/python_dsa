361cdb2b699336eb65e84a06b2347324960d8343
Baris Balli <barisbll.dev@gmail.com>
2022-04-21 10:12:52 +0200

    power of n is added

--- recursion/recursion.py
+++ recursion/recursion.py
@@ -38,6 +38,32 @@ def sum_of_digits(n):
         return remainder + sum_of_digits(int(n/10))
 
 
+def sum_of_digits_alternative(n):
+    if n < 0 or not isinstance(n, int):
+        return "Input n is invalid"
+
+    if n == 0:
+        return 0
+    else:
+        return int(n % 10) + sum_of_digits_alternative(int(n/10))
+
+
+# How to calculate power of a number using recursion
+def power_of_n(n, p):
+    if p < 0 or not isinstance(p, int):
+        return "Input n is invalid"
+
+    if p == 0:
+        return 1
+    else:
+        return n * power_of_n(n, p-1)
+
+
+# How to find GCD (Greatest Common Divisor) of two numbers using recursion
+def GCD(n1, n2):
+    pass
+
+
 def main():
     # factorial tests
     assert factorial(5) == 120, "Error"
@@ -57,13 +83,21 @@ def main():
     assert fibonacci(-4) == "Input n is invalid", "Error"
 
     # sum of digits test
-    assert sum_of_digits(945) == 18, "Error"
-    assert sum_of_digits(5) == 5, "Error"
-    assert sum_of_digits(729) == 18, "Error"
-    assert sum_of_digits(729987) == 42, "Error"
-    assert sum_of_digits(10) == 1, "Error"
-    assert sum_of_digits(29.3) == "Input n is invalid", "Error"
-    assert sum_of_digits(-4) == "Input n is invalid", "Error"
+    assert sum_of_digits_alternative(945) == 18, "Error"
+    assert sum_of_digits_alternative(5) == 5, "Error"
+    assert sum_of_digits_alternative(729) == 18, "Error"
+    assert sum_of_digits_alternative(729987) == 42, "Error"
+    assert sum_of_digits_alternative(10) == 1, "Error"
+    assert sum_of_digits_alternative(29.3) == "Input n is invalid", "Error"
+    assert sum_of_digits_alternative(-4) == "Input n is invalid", "Error"
+
+    # power of n tests
+    assert power_of_n(2, 3) == 8, "Error"
+    assert power_of_n(5, 3) == 125, "Error"
+    assert power_of_n(17, 2) == 289, "Error"
+    assert power_of_n(9, 3) == 729, "Error"
+    assert power_of_n(29.3, 1/2) == "Input n is invalid", "Error"
+    assert power_of_n(-4, -4) == "Input n is invalid", "Error"
 
 
 if __name__ == "__main__":
